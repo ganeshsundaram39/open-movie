801dd8fcb0b8fbde6c768e20b7115215a6db3d49
ganeshsundaram39 <ganeshsundaram39@gmail.com>
2018-05-10 23:00:03 +0530

    background look

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,28 @@
 import  'jquery';
-console.log($('h1'));
+
+$('#search__type').on('click',function(e){
+    e.preventDefault();
+
+    if($(this).val()==="id"){
+
+        $(this).val("title");
+        $(this).text("By IMDb ID");
+
+        $('[name="id"]').hide();
+        $('.search__bar div').fadeIn("slow");
+
+    } else if($(this).val()==="title"){
+
+        $(this).val("id");
+        $(this).text("By Title");
+
+        $('.search__bar div').hide();
+        $('[name="id"]').fadeIn("slow");
+    }
+});
+
+$(function(){
+    setInterval(function(){
+        $('.container.omovie').fadeIn("slow");
+    },1500);
+});
